a76ae0aa5aef13d6730fde6fb21e56bda16d974b
Juan Manuel Barreneche <jumanbar@gmail.com>
2011-11-19 20:35:13 -0200

    Arranca...

---
+++ matrixPower.R
@@ -0,0 +1,36 @@
+rw <- function(Po, t) {
+	N <- length(Po)
+	mat <- makeMat(N)
+	out <- matrix.power(t(mat), t) %*% Po
+	return(out)
+}
+
+makeMat <- function(N) {
+	mat <- matrix(0, N, N)
+	for(i in 1:(N-1)) {
+		mat[i, i+1] <- .5
+		mat[i+1, i] <- .5
+	}
+	mat[1,2] <- 1
+	mat[N,N-1] <- 1
+	return(mat)
+}
+
+
+matrix.power <- function(mat, t)
+{
+  # test if mat is a square matrix
+  # treat n < 0 and n = 0 -- this is left as an exercise
+  # trap non-integer n and return an error
+  if (t == 1) return(mat)
+  result <- diag(1, ncol(mat))
+  while (t > 0) {
+    if (t %% 2 != 0) {
+      result <- result %*% mat
+      t <- t - 1
+    }
+    mat <- mat %*% mat
+    t <- t / 2
+  }
+  return(result)
+}
---
+++ sample-raster-random-pts.R
@@ -0,0 +1,79 @@
+# Esto me anda con R 2.12.1 (2010-12-16), con los paquetes "raster" y "rgdal"
+# instalados.
+
+# Si hace falta instalarlos:
+# install.packages('raster')
+# install.packages('rgdal')
+
+# Cargar el paquete:
+library(raster)
+
+### PARA GENERAR LAS COORDENADAS DE LOS PUNTOS ###
+
+# Input del usuario:
+# Archivo con una capa raster:
+archivo <- 'prec10_44.tif'
+# Número de puntos a muestrear:
+npts <- 60000
+
+# Importar el archivo:
+r <- raster(archivo)
+
+# Límites de la capa (coordenadas):
+xmin <- r@extent@xmin
+ymin <- r@extent@ymin
+xmax <- r@extent@ymax
+ymax <- r@extent@ymax
+xmax <- r@extent@xmax
+
+# Pasos para generar coordenadas aleatorias:
+xcor <- runif(npts, xmin, xmax)    # 1 - Coordenadas x aleatorias
+ycor <- runif(npts, ymin, ymax)    # 2 - Coordenadas y aleatorias
+xy   <- data.frame(x=xcor, y=ycor) # 3 - Crea tabla con coordenadas
+
+# Para ver cuantos valores no NA ("buenos"):
+tmp <- extract(r, xy) # 4 - Extrae los valores de esas coordenadas
+bad <- is.na(tmp)     # 5 - Cuáles son los "malos"
+xy  <- xy[!bad,]      # 6 - Elimina los "malos"
+nrow(xy) # Número de valores "buenos"
+
+# De repente acá conviene pobar varias veces hasta que la cantidad de valores
+# "buenos" sea respetable... (para maxent, con 5 mil está sobrado creo). No
+# se puede determinar de antemano cuántos buenos se van a conseguir en cada
+# generación de coordenadas aleatorias, ya que depende de cuántos puntos
+# caen en el agua y cuántos no.
+# Una posible opción (que no está puesta acá) es ir acumulando puntos hasta
+# llegar a algo aceptable (repitiendo los pasos 1 al 6 en combinación con
+# rbind).
+
+# El objeto "xy" es el que tiene las coordenadas a muestrear, es recomendable
+# guardarlo en un archivo de texto:
+write.csv2(xy, file='xy.csv')
+
+
+### EXTRAER LOS VALORES PARA OTRAS CAPAS ###
+
+# Input del usuario:
+# Cuántas capas son en total?:
+ncapas <- 1
+# Nombres de las capas:
+nombres <- c('c1')
+
+# Si es necesario, importar la tabla con las coordenadas:
+# xy <- read.csv2('xy.csv')
+
+# Tabla final (tendrá los valores de todas las capas):
+tabla <- as.data.frame(matrix(0, nrow=nrow(xy), ncol=ncapas + 2))
+tabla[, 1:2] <- xy
+names(tabla) <- c('x', 'y', nombres)
+
+# Para extraer todos los valores de todas las capas, se toma el objeto xy y
+# la función extract (paquete raster) para obtenerlos (pasos a repetir):
+
+nc      <- 1                 # 1 - Número de capa (NO REPETIR!)
+capa    <- 'capa.tiff'       # 2 - Nombre del archivo de la capa
+r       <- raster(capa)      # 3 - Importar
+valores <- extract(capa, xy) # 4 - Extraer valores
+tabla[, nc + 2] <- valores   # 5 - Actualizar tabla
+nc <- nc + 1                 # 6 - Actualizar nc
+
