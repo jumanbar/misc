c3be156041242f219e70312e6acd0f15e4785bf8
Juan Manuel Barreneche <jumanbar@gmail.com>
2014-05-23 13:53:37 -0300

    Reacomodo de archivos

--- Python/hanoi.py
+++ Python/hanoi.py
@@ -3,27 +3,42 @@
 Created on Thu May 22 14:44:31 2014
 
 @author: Juan Manuel Barreneche
+
+Proyecto: torres de Hanoi.
 """
 
 import sys
 
 class TowerSet:
+    """
+    TowerSet(n)
+    Un TowerSet es un set de 3 torres de Hanoi de n piezas.
+    """
     n_mov = 0
     def __init__(self, n):
         self.torres = {'t1': range(n, 0, -1), 't2': [], 't3': []}
         self.update_ts()
         self.n = n
+    def __repr__(self):
+        out = 'Movimientos: ' + str(self.n_mov) + \
+          '\nt1: ' + '-'.join([str(n) for n in self.t1]) + \
+          '\nt2: ' + '-'.join([str(n) for n in self.t2]) + \
+          '\nt3: ' + '-'.join([str(n) for n in self.t3])
+        return out
+    def __str__(self):
+        return self.__repr__()
     def update_ts(self):
         self.t1 = self.torres['t1']
         self.t2 = self.torres['t2']
         self.t3 = self.torres['t3']
-    def show(self):
-        #print '#Piezas: ' + str(self.n)
-        print 'Movimientos: ' + str(self.n_mov)
-        print 't1: ' + '-'.join([str(n) for n in self.t1])
-        print 't2: ' + '-'.join([str(n) for n in self.t2])
-        print 't3: ' + '-'.join([str(n) for n in self.t3])
-    def move(self, ini, fin):
+    def move(self, *args):
+        if len(args) == 1:
+            args = str(args)
+            if len(args
+            ini = 't' + args[0]
+            fin = 't' + args[1]
+
+
         fini = self.torres[ini][-1]
         print str(fini) + ': ' + ini + ' ---> ' + fin
         if len(self.torres[fin]) > 0:
@@ -41,6 +56,23 @@ class TowerSet:
         self.n_mov = self.n_mov + 1
         self.show()
         print ''
+    def mover(self, dirx):
+        fini = self.torres[ini][-1]
+        print str(fini) + ': ' + ini + ' ---> ' + fin
+        if len(self.torres[fin]) > 0:
+            ffin = self.torres[fin][-1]
+        else:
+            ffin = fini + 1
+        if fini > ffin:
+            print 'Error:'
+            print 'La ficha ' + str(fini) + ' no se puede colocar encima de la ficha ' + str(ffin) + '.'
+            print 'No se puede realizar el movimiento.'
+            return 0
+        self.torres[ini].remove(fini)
+        self.torres[fin].append(fini)
+        self.update_ts()
+        self.n_mov = self.n_mov + 1
+        print self
         
 def solve(t, n=-2, ini='t1', fin='t3'):
     if n == -2:
@@ -68,9 +100,7 @@ def hanoi(n):
 
 if __name__ == '__main__':
     print '[modulo hanoi importado]'
-    if len(sys.argv) > 1:
-        npiezas = int(sys.argv[1])
-    else:
-        npiezas = 3
-    hanoi(npiezas)
+    # if len(sys.argv) > 1:
+        # npiezas = int(sys.argv[1])
+        # hanoi(npiezas)
 
