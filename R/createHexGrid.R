d9743490a8bfb7c76436fef9dbc1ba294adba31d
Juan Manuel Barreneche <jumanbar@gmail.com>
2015-10-22 18:29:28 -0200

    Update createHexGrid.R

--- R/createHexGrid.R
+++ R/createHexGrid.R
@@ -1,18 +1,37 @@
-crtHexGrid <- function(d, Nx, Ny) {
-  xcoor <- seq(0, d * (Nx - 1), by=d)
-  xcoor <- rep(xcoor, Ny)
+#' Hexagonal grid creator
+#'
+#' Creates a grid made of hexagons (or equilateral triangles if you wish), with
+#' \code{Nx} by \code{Ny} points.
+#'
+#' @param d A single numeric value, the (minimum) distance between points and the side of the triangles.
+#'
+#' @param Nx integer: The number of points along the x axis.
+#'
+#' @param Ny integer: The number of points along the y axis.
+#'
+#' @value
+#'
+#'
+#' @example ...
+#'
+#'
+#'
+#'
+hex_grid <- function(d, Nx, Ny) {
+  xcoor <- seq(0, d * (Nx - 1), by = d/2)
+  xcoor <- rep.int(xcoor, Ny)
   xind  <- rep(1:Ny, each=Nx)
   p <- xind %% 2 == 0
   xcoor[p] <- xcoor[p] + d / 2
-  
+
   dy <- d * sin(pi / 3)
   ycoor <- seq(0, dy * (Ny - 1), by=dy)
   ycoor <- rep(ycoor, each=Nx)
-
+  
   return(cbind(xcoor, ycoor))
 }
 
-crtHexagon <- function(d) {
+hexagon <- function(d) {
   ang  <- seq(2 * pi / 6, 2 * pi, len=6)
   coor <- rbind(c(0, 0), cbind(d * cos(ang), d * sin(ang)))
   return(coor)
