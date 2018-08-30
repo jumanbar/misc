31b5991888fd36c225de093e04b4d3f895994e87
Juan Manuel Barreneche <jumanbar@gmail.com>
2018-08-30 16:28:40 -0300

    Create temp_LaPaloma.R
    Archivo para graficar los datos abiertos de Manta & Trinchin (La Paloma, Uruguay).
    Ver: http://www.seanoe.org/data/00445/55639/

---
+++ R/temp_LaPaloma.R
@@ -0,0 +1,14 @@
+library(readr)
+library(tidyverse)
+temp <- read_delim("../Downloads/57285.txt", "\t", 
+                   escape_double = FALSE, trim_ws = TRUE)
+
+names(temp)[c(4, 5)] <- c("hour", "Temp")
+
+temp <- mutate(temp, datetime = ymd_h(paste0(Year, "-", Month, "-", Day, " ", hour)))
+
+ggplot(temp, aes(x = datetime, y = Temp, col=month(temp$Month, label = T, abbr = F))) +
+  geom_point() + theme(legend.title=element_blank()) + 
+  scale_color_discrete() + 
+  xlab("Fecha") + ylab("Temperatura | 0.5m (ºC)") + 
+  ggtitle("La Paloma - 34.65ºS, 54.12ºW")
