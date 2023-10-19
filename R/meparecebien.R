c186fbad9add72324d0f8347155c5fea28f2caca
Juan Manuel Barreneche <jumanbar@gmail.com>
2023-10-19 17:36:34 -0300

    Mejor doc en meparecebien.R

--- R/meparecebien.R
+++ R/meparecebien.R
@@ -1,25 +1,35 @@
-# Función 100% inútil que hice para entretenerme
-
-mepa <- function(n_=4) {
-	x <- 'me parece bien'
-	if(n_ <= 1) {
-		x <- sub('me', 'te', x)
-		x <- sub('parece', 'parezca', x)
-		x <- paste('me parece bien que', x)
-	} else {
-		for(i in 1:(n_ - 1)) {
-			x <- c('me parece bien que', x)
-		}		
-		x <- gsub('parece', 'parezca', x)
-		x[(1:n_%%2) == 0] <- gsub('te', 'me', x[(1:n_ %% 2) == 0])
-		x[(1:n_%%2) == 1] <- gsub('me', 'te', x[(1:n_ %% 2) == 1])
-		x <- c('me parece bien que', x)
-		x <- paste(x, collapse=' ')
-	}
-	return(x)
+#' Me parece bien que te parezca bien
+#'
+#' Función 100% inútil que hice para entretenerme
+#'
+#' @param n Número de interaciones (integer)
+#'
+#' @return Character: un string con un diálogo de n ideas y vueltas entre dos
+#'   personas a las que les parece bien que a la otra persona le parezca bien.
+#' @export
+#'
+#' @examples
+#' mepa(1)
+#' mepa(2)
+#' mepa(4)
+#' # Escrbir en un archivito de texto:
+#' for(i in 1:6) cat(mepa(i), append=TRUE, file='mepa.txt', sep='\n')
+mepa <- function(n = 1L) {
+  x <- 'me parece bien'
+  if(n <= 1) {
+    x <- sub('me', 'te', x)
+    x <- sub('parece', 'parezca', x)
+    x <- paste('me parece bien que', x)
+  } else {
+    for(i in 1:(n - 1)) {
+      x <- c('me parece bien que', x)
+    }		
+    x <- gsub('parece', 'parezca', x)
+    x[(1:n%%2) == 0] <- gsub('te', 'me', x[(1:n %% 2) == 0])
+    x[(1:n%%2) == 1] <- gsub('me', 'te', x[(1:n %% 2) == 1])
+    x <- c('me parece bien que', x)
+    x <- paste(x, collapse=' ')
+  }
+  return(x)
 }
 
-### EJEMPLOS:
-# mepa(8)
-# for(i in 1:6) cat(mepa(i), append=TRUE, file='mepa.txt', sep='\n')
-### Luego abrir el archivo mepa.txt de la carpeta de trabajo...
